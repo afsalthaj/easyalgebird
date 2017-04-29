aedde04a6174cdf9991faaf02c3d9d9f5c61a0ce
Afsal Thaj <afsal.taj06@gmail.com>
2017-04-29 19:57:58 +1000

    Update EasyAlgebird.scala

--- src/main/scala/com/thajaf/easyalgebird/EasyAlgebird.scala
+++ src/main/scala/com/thajaf/easyalgebird/EasyAlgebird.scala
@@ -27,7 +27,7 @@ object EasyAlgebird {
     data:         TypedPipe[T],
     primaryKey:   T => P,
     agg:          Aggregator[T, B, List[AttributeValue[S, V]]],
-    reducerCount: Int                                          = 108
+    reducerCount: Int = 108
   ): TypedPipe[AggregatedData[P, S, V]] =
     data.groupBy( primaryKey )
       .aggregate( agg )
@@ -35,17 +35,9 @@ object EasyAlgebird {
       .toTypedPipe
       .map { case ( key, av ) => AggregatedData( key, av ) }
 
-  /** A handy method to convert a map
-    * that can exist in a data to List of `AttributeValue`
-    * where the attribute name and its value are clearly specified.
-    * Pivoting utilities are based on this.
-    */
   private def toListOfApv[S, V]( data: Map[S, V] ): List[AttributeValue[S, V]] =
     data.toList.map { case ( a, v ) => AttributeValue( a, v ) }
-
-  /** Method to transform the primary key in the
-    * aggregated data. This can be a requirement
-    */
+  
   def transformAggregatedData[T <: ThriftStruct, P: Ordering, S, V](
     aggregatedData: TypedPipe[AggregatedData[P, S, V]],
     f:              P => T
@@ -56,7 +48,7 @@ object EasyAlgebird {
     * transformation based on aggregated data and emit all of them
     * as one data set.
     */
-  private def addMoreFeaturesToAggData[T <: ThriftStruct, P: Ordering, S, V](
+  private def addMoreAttributeValue[T <: ThriftStruct, P: Ordering, S, V](
     aggregatedData: TypedPipe[AggregatedData[P, S, V]],
     f:              AggregatedData[P, S, V] => List[AttributeValue[S, V]]
   ) = aggregatedData.map { agg => AggregatedData( agg.primaryKey, agg.list ++ f( agg ) ) }
@@ -64,7 +56,7 @@ object EasyAlgebird {
   /** Method to convert an aggregated data to
     * pivoted data
     */
-  def pivotAggregatedData[T <: Product, P, S, V](
+  def transposeAggData[T <: Product, P, S, V](
     typedPipe:         TypedPipe[AggregatedData[P, S, V]],
     f:                 ( P, AttributeValue[S, V] ) => T,
     pivotBasedOnField: AggregatedData[P, S, V] => List[AttributeValue[S, V]] = ( agg: AggregatedData[P, S, V] ) => agg.list
@@ -77,7 +69,7 @@ object EasyAlgebird {
   }
 
   /** Pivot Simple Data on Map, further abstraction was possible, but didn't do */
-  def pivotDataOnSimpleMap[T <: Product, U <: Product, S, V](
+  def pivotOnMap[T <: Product, U <: Product, S, V](
     typedPipe:         TypedPipe[T],
     f:                 ( T, AttributeValue[S, V] ) => U,
     pivotBasedOnField: T => Map[S, V]
@@ -85,7 +77,7 @@ object EasyAlgebird {
     pivotAggregatedData( typedPipe.map( t => AggregatedData( t, toListOfApv( pivotBasedOnField( t ) ) ) ), f )
 
   /** to pivot based a data based on a field, that can potentially be a list, further abstraction was possible though */
-  def pivotDataOnArray[T1 <: Product, T2 <: Product, V](
+  def pivotOnArray[T1 <: Product, T2 <: Product, V](
     typedPipe:       TypedPipe[T1],
     pivotBasedField: T1 => List[V],
     f:               PivotedData[T1, V] => T2
@@ -93,20 +85,20 @@ object EasyAlgebird {
 
   /** find max value given an aggregated data set
     */
-  def findMaxInAggregation[T <: ThriftStruct, M: Ordering](
+  def findMax[T <: ThriftStruct, M: Ordering](
     maxOfField: T => M
   ): Aggregator[T, T, M] = maxBy[T, M]( maxOfField ).andThenPresent( maxOfField )
 
   /** find min value given an aggregated data set
     */
-  def findMinInAggregation[T <: ThriftStruct, M: Ordering](
+  def findMin[T <: ThriftStruct, M: Ordering](
     minOfField: T => M
   ): Aggregator[T, T, M] = minBy[T, M]( minOfField ).andThenPresent( minOfField )
 
   /** Count the number of rows, given
     * an aggregated data set, based on a condition
     */
-  def countValue[T <: ThriftStruct](
+  def countEvent[T <: ThriftStruct](
     condition:   T => Boolean,
     measureName: Measure
   ) = com.twitter.algebird.Aggregator.count( condition ).andThenPresent( t => AttributeValue( measureName.toString, t ) )
@@ -114,16 +106,12 @@ object EasyAlgebird {
   /** sum of a field in an aggregated
     * data set
     */
-  def sumValue[T <: ThriftStruct](
+  def sum[T <: ThriftStruct](
     field:       T => Long,
     measureName: Measure
   ): MonoidAggregator[T, Long, AttributeValue[String, Long]] = prepareMonoid( field ).andThenPresent( t => AttributeValue( measureName.toString, t ) )
-
-  /** `Ordering` implementation for a `ThriftStruct`
-    * This is an already implemented version in util, except
-    * we get rid of thriftstruct dependency here.
-    */
-  class ThriftOrder[T <: Product] extends Ordering[T] {
+  
+  class ProductOrder[T <: Product] extends Ordering[T] {
     def compare( x: T, y: T ): Int =
       ( 0 to x.productArity - 1 )
         .map( i => compareItems( x.productElement( i ), y.productElement( i ) ) )
@@ -156,11 +144,6 @@ object EasyAlgebird {
   }
 }
 
-/** Algebird aggregations always
-  * play with nested tuple, and shapeless
-  * will be a good tool to flatten
-  * the nested tuple
-  */
 object Flatten {
   import shapeless._
   import shapeless.ops.tuple.FlatMapper
